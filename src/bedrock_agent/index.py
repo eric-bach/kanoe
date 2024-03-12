869afe998e478920bd60766f722bdadbe9bbf19d
Eric Bach <bach.eric@gmail.com>
2024-03-11 19:17:45 -0600

    Update

--- src/bedrock_agent/index.py
+++ src/bedrock_agent/index.py
@@ -1,5 +1,4 @@
 import os
-import json
 import requests
 from aws_lambda_powertools import Logger
 
@@ -7,51 +6,49 @@ API_GATEWAY_URL = os.environ["API_GATEWAY_URL"]
 
 logger = Logger()
 
-def return_member_info(memberNumber):
-    url = f"{API_GATEWAY_URL}member/{memberNumber}"
-    headers = {
-        "Content-Type": "application/json"
-    }
-
-    print(url)
-
-    response = requests.get(url, headers=headers)
-    response.raise_for_status()
-
-    print(response.json())
-
-    return response.json()
-
 @logger.inject_lambda_context(log_event=True)
 def handler(event, context):
     api_path = event['apiPath']
-    
-    if api_path == '/member/{MemberNumber}':
+
+    data = "Sorry, please try again later."
+
+    if (api_path == '/member/{memberNumber}'):
         parameters = event['parameters']
         for parameter in parameters:
             if parameter["name"] == "MemberNumber":
                 memberNumber = parameter["value"]
-        body = return_member_info(memberNumber)
-    else:
-        body = {"{} is not a valid api, try another one.".format(api_path)}
+        
+        url = f"{API_GATEWAY_URL}member/{memberNumber}"
+        headers = {
+            "content-type": "application/json"
+        }
+
+        response = requests.get(url, headers=headers)
+        response.raise_for_status()
+
+        logger.info("Response", response.json())
+
+        data = response.json()
+
 
     # https://docs.aws.amazon.com/bedrock/latest/userguide/agents-lambda.html
-    response_body = {
-        'application/json': {
-            'body': body
-        }
-    }
-    response = {
+    result = {
         'messageVersion': '1.0',
         'response': {
             'actionGroup': event['actionGroup'],
             'apiPath': event['apiPath'],
             'httpMethod': event['httpMethod'],
             'httpStatusCode': 200,
-            'responseBody': response_body,
+            'responseBody': {
+                'application/json': {
+                    'body': data
+                }
+            },
             'sessionAttributes': {},
             'promptSessionAttributes': {}
         }
     }
 
-    return response
+    logger.info("Result", result)
+
+    return result
